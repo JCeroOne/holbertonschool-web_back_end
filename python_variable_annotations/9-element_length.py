4ce42492eed7492e53c2231009b1073f1ece5763
JCeroOne <jceroone@gmail.com>
2026-04-08 18:24:14 +0000

    Update

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines the 'element_length' function."""
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Returns the contents and length of each element in a list.
+
+    Parameters:
+        lst (list[*]): The list.
+
+    Returns: (*, int) Exactly what it says above."""
+
+    return [(i, len(i)) for i in lst]
