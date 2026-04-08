6621e4eadb0b8d7f250af7e6057ef581d3a58ed7
JCeroOne <jceroone@gmail.com>
2026-04-08 17:59:21 +0000

    Update

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines the 'sum_list' function."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Adds all the values of a list of floats together.
+
+    Parameters:
+        input_list (list[float]): The list of numbers.
+
+    Returns: (float) The sum of all the values."""
+
+    return sum(input_list)
