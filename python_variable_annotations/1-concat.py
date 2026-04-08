79160bb882e9d48678cb2b0a7525fa10d782a7ad
JCeroOne <jceroone@gmail.com>
2026-04-08 17:32:06 +0000

    Update

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines the 'concat' function, which concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings.
+
+    Parameters:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns: (str) The concatenated string."""
+
+    return a + b
