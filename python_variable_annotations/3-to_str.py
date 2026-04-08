d0a817539984ceac94c2d186faf52bb560f7b88d
JCeroOne <jceroone@gmail.com>
2026-04-08 17:49:20 +0000

    Update

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines the 'to_str' function."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a number.
+
+    Parameters:
+        n (float): A number.
+
+    Returns: (str) The string representation of the number."""
+
+    return str(n)
