0daec8d805c3c0b96670c47a6494d458827c645b
JCeroOne <jceroone@gmail.com>
2026-04-08 17:39:18 +0000

    Update

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+
+"""Defines the 'floor' function."""
+
+
+def floor(n: float) -> int:
+    """Removes the decimal places of a number.
+
+    Parameters:
+        n (float): The number to truncate.
+
+    Returns: (int) The truncated number."""
+
+    return math.floor(n)
