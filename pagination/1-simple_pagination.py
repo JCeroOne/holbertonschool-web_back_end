181fd0c92764a27ddd02ca4f7d2092d04bd129a2
root <root@ip-10-42-11-73.ec2.internal>
2026-04-27 02:08:55 +0000

    Update

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -40,8 +40,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Retrieves the entries in a given page."""
-        assert(isinstance(page, int) and isinstance(page_size, int))
-        assert(page > 0 and page_size > 0)
+        assert (isinstance(page, int) and isinstance(page_size, int))
+        assert (page > 0 and page_size > 0)
 
         bounds = index_range(page, page_size)
         rows = []
