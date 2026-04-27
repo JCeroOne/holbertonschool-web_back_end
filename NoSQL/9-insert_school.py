37dc0ae0430794eccf48ff43774a30b4db893993
JCeroOne <jceroone@gmail.com>
2026-04-26 21:02:41 -0600

    Update

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines the insert_school function."""
+
+
+def insert_school(mongo_collection, *kwargs):
+    """Inserts a new document into the given collection."""
+
+    mongo_collection.insert_one(kwargs)
+
+    return mongo_collection.find_one(kwargs)["_id"]
