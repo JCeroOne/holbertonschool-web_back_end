c84d8b5e2e26b38baa60f3320c5c8582238d65d2
JCeroOne <jceroone@gmail.com>
2026-04-26 21:19:30 -0600

    Update

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,13 +7,16 @@ if name == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     nginx = client.logs.nginx
 
-    print(f"{nginx.count()} logs")
+    print("{} logs".format(nginx.count()))
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for m in methods:
-        print(f"\tmethod {m}: {nginx.find({'method': method}).count()}")
+        print(f"\tmethod {}: {}".format(
+            m,
+            nginx.find({"method": m}).count()
+        ))
 
     print("{} status check".format(
         nginx.find({
